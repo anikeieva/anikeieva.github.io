a22c8be29c30ee4b4bd9795b658ce34a351deeda
anikeieva <alyonaoooooooo@gmail.com>
2018-05-22 16:57:06 +0300

    Added hover to portfolio section, enlish text to about-me-section, continued to doadapted site

---
+++ app/js/portfolio-section.js
@@ -0,0 +1,24 @@
+function ready() {
+  let portfolioSlide = document.getElementsByClassName('portfolio__item')[0];
+  let portfolioHoverText = document.getElementsByClassName('portfolio-section__hover-text')[0];
+  let portfolioSection = document.getElementsByClassName('portfolio-section')[0];
+
+  console.log(portfolioHoverText);
+
+  function hoverPortfolioSlide() {
+
+    portfolioHoverText.classList.toggle('visible');
+
+    let portfolioHoverTextWidth = portfolioHoverText.offsetWidth;
+    let portfolioHoverTextHeight = portfolioHoverText.offsetHeight;
+
+    portfolioSection.classList.toggle('portfolio-section_hover');
+    portfolioHoverText.style.left = `calc(50% - ${portfolioHoverTextWidth/2}px)`;
+    portfolioHoverText.style.top = `calc(50% - ${portfolioHoverTextHeight/2}px)`;
+  }
+
+  portfolioSlide.addEventListener('mouseover', hoverPortfolioSlide);
+  portfolioSlide.addEventListener('mouseout', hoverPortfolioSlide);
+}
+
+document.addEventListener("DOMContentLoaded", ready);
