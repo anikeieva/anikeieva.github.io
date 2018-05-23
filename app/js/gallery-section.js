d7363df91ce51c83e5041b1a95461441f8215aee
anikeieva <alyonaoooooooo@gmail.com>
2018-05-23 16:10:48 +0300

    finished adapted version, fixed some bugs

--- app/js/gallery-section.js
+++ app/js/gallery-section.js
@@ -2,20 +2,35 @@ function ready() {
   let galleryBtnRight = document.getElementsByClassName('gallery-section__angle-button-right')[0];
   let galleryBtnLeft = document.getElementsByClassName('gallery-section__angle-button-left')[0];
   let gallerySslide = document.getElementsByClassName('gallery-section__item')[0];
-  const gallerySslidesArray = [
-    'img/gallery/it-школяр/30171219_169344820445798_4335574628847642808_o.jpg',
-    'img/gallery/it-школяр/29983054_169344823779131_2652349194549814631_o.jpg',
-    'img/gallery/it-школяр/29983211_169345093779104_855175623059303314_o.jpg',
-    'img/gallery/it-школяр/29983262_169344933779120_3610492867027237624_o.jpg',
-    'img/gallery/it-школяр/29983363_169345110445769_6292697743476448005_o.jpg',
-    'img/gallery/it-школяр/29983413_169344963779117_7462035664258350859_o.jpg',
-    'img/gallery/it-школяр/30072747_169344957112451_2575314177981744514_o.jpg',
-    'img/gallery/it-школяр/30167999_169344943779119_8144949423898715383_o.jpg',
-    'img/gallery/it-школяр/30168003_169344810445799_4238244016094024780_o.jpg',
-    'img/gallery/it-школяр/30168014_169345100445770_7985281765583073547_o.jpg',
-    'img/gallery/it-школяр/30171979_169345083779105_197143806832624855_o.jpg',
-    'img/gallery/it-школяр/30425750_169344803779133_7107726855062376792_o.jpg'
+
+  let gallerySslidesArray = [
+    'img/gallery/it-shkolyar/1.jpg',
+    'img/gallery/it-shkolyar/2.jpg',
+    'img/gallery/it-shkolyar/3.jpg',
+    'img/gallery/it-shkolyar/4.jpg',
+    'img/gallery/it-shkolyar/5.jpg',
+    'img/gallery/it-shkolyar/6.jpg',
+    'img/gallery/it-shkolyar/7.jpg',
+    'img/gallery/it-shkolyar/8.jpg',
+    'img/gallery/it-shkolyar/9.jpg',
+    'img/gallery/it-shkolyar/10.jpg'
   ];
+
+  if(document.getElementsByClassName('ua-page')[0]) {
+
+    let newArray = [];
+
+    gallerySslidesArray.map( (item) => {
+      let itemSend = item.slice(0),
+          newSymb = '../',
+          result = newSymb + itemSend;
+
+      newArray.push(result);
+    });
+
+    gallerySslidesArray = newArray;
+  }
+
   let counter = 0;
 
   function changingSlideRight() {
@@ -23,7 +38,6 @@ function ready() {
       counter = -1;
     }
     ++counter;
-    // console.log(counter);
 
     gallerySslidesArray.filter( (item, index) => {
       if(index === counter) {
@@ -37,7 +51,6 @@ function ready() {
       counter = gallerySslidesArray.length;
     }
     --counter;
-    // console.log(counter);
 
     gallerySslidesArray.filter( (item, index) => {
       if(index === counter) {
--- app/js/sideBarMenu.js
+++ app/js/sideBarMenu.js
@@ -2,16 +2,14 @@ function ready() {
   var menu_toggleBtn = document.getElementsByClassName('menu-container__toggle-btn')[0];
   var menu_icon = document.getElementsByClassName('menu-container__icon')[0];
   var menu_sideBar = document.getElementsByClassName('menu-container__side-bar')[0];
-  var mainContainer = document.getElementsByClassName('main-container')[0];
-  var header_lineSide = document.getElementsByClassName('header__line-side')[0];
+  var faBars = document.getElementsByClassName('fa-bars')[0];
 
   function activeMenu() {
 
     menu_sideBar.classList.toggle('active');
     menu_icon.classList.toggle('fa-bars');
     menu_icon.classList.toggle('fa-times');
-    // header_lineSide.classList.toggle('header__line-side_left');
-    // mainContainer.classList.toggle('main-container_left');
+    faBars.classList.toggle('no-active-bars');
 
   }
 
@@ -23,8 +21,7 @@ function ready() {
     menu_sideBar.classList.toggle('active');
     menu_icon.classList.toggle('fa-bars');
     menu_icon.classList.toggle('fa-times');
-    // header_lineSide.classList.toggle('header__line-side_left');
-    // mainContainer.classList.toggle('main-container_left');
+    faBars.classList.toggle('no-active-bars');
 
   }
 
