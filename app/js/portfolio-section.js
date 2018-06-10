ee52456496df26fb4faba5e40d794cad1659f6a2
anikeieva <alyonaoooooooo@gmail.com>
2018-06-10 12:34:48 +0300

    Fixed cross browzer fit-content

--- app/js/portfolio-section.js
+++ app/js/portfolio-section.js
@@ -7,12 +7,8 @@ function ready() {
 
     portfolioHoverText.classList.toggle('visible');
 
-    let portfolioHoverTextWidth = portfolioHoverText.offsetWidth;
-    let portfolioHoverTextHeight = portfolioHoverText.offsetHeight;
-
     portfolioSection.classList.toggle('portfolio-section_hover');
-    portfolioHoverText.style.left = `calc(50% - ${portfolioHoverTextWidth/2}px)`;
-    portfolioHoverText.style.top = `calc(50% - ${portfolioHoverTextHeight/2}px)`;
+
   }
 
   portfolioSlide.addEventListener('mouseover', hoverPortfolioSlide);
